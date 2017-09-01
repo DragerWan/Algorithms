e1dbb4272088fc35fe1411a1927dba39aad2d9ef
DragerWan <18128843927@163.com>
2017-09-01 16:55:57 +0800

    Add quick-union

---
+++ src/union_find/QuickUnion.java
@@ -0,0 +1,24 @@
+package union_find;
+
+public class QuickUnion {
+	private int[] id;
+	public QuickUnion(int n){
+		id = new int[n];
+		for(int i = 0; i < n; i++)
+			id[i] = i;
+	}
+	public int root(int n){
+		while(id[n] != n)
+			n = id[n];
+		return n;
+	}
+	public void union(int a, int b){
+		int rootA = root(a);
+		int rootB = root(b);
+		if(rootA != rootB)
+			id[rootA] = rootB;
+	}
+	public boolean find(int a, int b){
+		return root(a) == root(b);
+	}
+}
