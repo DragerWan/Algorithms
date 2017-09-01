c55ec56e50c77062a82febc8dd8c05a9433c36bd
DragerWan <18128843927@163.com>
2017-09-01 15:35:59 +0800

    add quick-find algorithm

---
+++ src/union_find/QuickFind.java
@@ -0,0 +1,21 @@
+package union_find;
+
+public class QuickFind {
+	private int[] id;
+	public QuickFind(int n){
+		id = new int[n];
+		for(int i = 0; i < 1; i++)
+			id[i] = i;
+	}
+	public void union(int a, int b){
+		int aa = id[a];
+		int bb = id[b];
+		for(int i = 0; i < id.length; i ++){
+			if(id[i] == aa)
+				id[i] = bb;
+		}
+	}
+	public boolean find(int a, int b){
+		return id[a] == id[b];
+	}
+}
